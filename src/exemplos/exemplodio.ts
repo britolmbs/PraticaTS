9b23ee37e63cd4557cb7c0b8cef0c0767739f2ea
britolmbs <britolmbs@hotmail.com>
2022-08-05 10:41:35 -0300

    Aula de patrica de TypeScript

---
+++ src/exemplos/exemplodio.ts
@@ -0,0 +1,20 @@
+let butao = document.getElementById('botao');
+let input1 = document.getElementById('input1') as HTMLInputElement;
+let input2 = document.getElementById('input2') as HTMLInputElement;
+
+ function adicionarNumeros (numero1: number, numero2: number, devePrintar: boolean, frase: string){
+   let resultado = numero1 + numero2
+   if (devePrintar){
+      console.log(frase + resultado)
+   }
+    return numero1 + numero2;
+ }
+let devePrintar = true;
+let frase = 'O valor é: '
+
+if (butao){
+ butao.addEventListener('click', () => {
+    if(input1 && input2){
+adicionarNumeros(Number(input1.value), Number(input2.value), devePrintar, frase) }
+ })
+}
